d8f340c8d75a30d210349fe546ce55179cb0e9f9
rohidas-gowda <rohidasgowda@gmail.com>
2023-04-11 15:41:11 +0530

    initial commit

---
+++ src/components/Abrasives.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Abrasives() {
+  return (
+    <div className="bg-gray-200 h-48 rounded-lg m-2 shadow-xl">
+      <h2 className="text-2xl my-8">Abrasives</h2>
+      <button className="bg-blue-500 p-2 w-32 rounded-lg my-2 font-semibold text-white">
+        Check
+      </button>
+    </div>
+  );
+}
+
+export default Abrasives;
---
+++ src/components/Sectors.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Abrasives from './Abrasives';
+
+function Sectors() {
+  return (
+    <div className='grid gap-4 grid-cols-4'>
+        <Abrasives />
+    </div>
+  )
+}
+
+export default Sectors
